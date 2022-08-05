71904551f6aae85be8624dfc2fbfde28122647a7
Marat <zinatulinmarat@gmail.com>
2022-08-05 22:22:57 +0500

    cart

--- frontend/src/app/components/router/cart/cart-item/cart-item.component.ts
+++ frontend/src/app/components/router/cart/cart-item/cart-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from './../../../../services/product/product.service';
-import { filter } from 'rxjs';
+import { filter, fromEvent, map, pipe } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cart-item',
@@ -11,6 +12,10 @@ export class CartItemComponent implements OnInit {
 
   @Input() item: Product;
   fileUrl = null;
+  rowQty = [];
+  selectContState = false;
+  activeIndex: number;
+  subs: Subscription;
 
   constructor() { }
 
@@ -19,6 +24,44 @@ export class CartItemComponent implements OnInit {
     if (fileItem.length > 0) {
       this.fileUrl = fileItem[0].path;
     }
+    for (let i = 0; i < 10; i++) {
+      this.rowQty.push({
+        q: i+1,
+        s: String(i+1) === String(this.item.qty)
+      });
+      if (String(i+1) === String(this.item.qty)) {
+        this.activeIndex = i;
+      }
+    }
+  }
+
+  selectQty() {
+    this.selectContState = true;
+    setTimeout(() => {
+      this.subs = fromEvent(document, 'click')
+      .pipe(
+        map((event: MouseEvent) => event.target)
+      )
+      .subscribe((target: HTMLElement) => {
+        if (!target.closest('.product-row-qty')) {
+          this.selectContState = false;
+          this.subs.unsubscribe();
+        }
+      });
+    }, 100)
+  }
+
+  onSelectQty(qty, selectItem: HTMLElement, index: number) {
+    this.item.qty = qty.q;
+    document.querySelector('div[select="selected"]').setAttribute('select', 'none');
+    selectItem.setAttribute('select', 'selected');
+    this.rowQty[this.activeIndex].s = false;
+    this.activeIndex = index;
+    this.rowQty[index].s = true;
+    this.subs.unsubscribe();
+    setTimeout(()=> {
+      this.selectContState = false;
+    }, 10);
   }
 
 }
--- frontend/src/app/components/router/cart/cart.component.ts
+++ frontend/src/app/components/router/cart/cart.component.ts
@@ -47,4 +47,5 @@ export class CartComponent implements OnInit {
     
   }
 
+
 }
